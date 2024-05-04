765d15a37e069af8f18c71e06c630b958958de1f
Diego Romário <diego.romario@outlook.com>
2024-05-04 16:14:40 -0300

    add web api

---
+++ src/MMMovies.Api/Program.cs
@@ -0,0 +1,43 @@
+using FastEndpoints;
+using FastEndpoints.Swagger;
+using Serilog;
+using System.Reflection;
+using MMMovies.Movies;
+
+var logger = Log.Logger = new LoggerConfiguration().Enrich.FromLogContext()
+                                                   .WriteTo.Console()
+                                                   .CreateLogger();
+
+logger.Information("Starting web host");
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((_, config) => config.ReadFrom.Configuration(builder.Configuration));
+
+builder.Services.AddFastEndpoints()
+                //.AddAuthenticationJwtBearer(s => s.SigningKey = builder.Configuration["Auth:JwtSecret"]!)
+                //.AddAuthorization()
+                .SwaggerDocument();
+
+// Add Module Services
+List<Assembly> mediatRAssemblies = [typeof(Program).Assembly];
+builder.Services.AddMovieModuleServices(builder.Configuration, logger, mediatRAssemblies);
+
+// Set up MediatR
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies([.. mediatRAssemblies]));
+//builder.Services.AddMediatRLoggingBehavior();
+//builder.Services.AddMediatRFluentValidationBehavior();
+//builder.Services.AddValidatorsFromAssemblyContaining<AddItemToCartCommandValidator>();
+// Add MediatR Domain Event Dispatcher
+//builder.Services.AddScoped<IDomainEventDispatcher, MediatRDomainEventDispatcher>();
+
+var app = builder.Build();
+
+//app.UseAuthentication()
+//   .UseAuthorization();
+
+app.UseFastEndpoints()
+   .UseSwaggerGen();
+
+app.Run();
+public partial class Program { } // needed for tests and mediatR 
---
+++ src/MMMovies.Movies/Services/MovieService.cs
@@ -0,0 +1,57 @@
+﻿using MMMovies.Movies.Data.Repositories;
+using MMMovies.Movies.Domain;
+using MMMovies.Movies.DTOs;
+
+namespace MMMovies.Movies.Services;
+internal class MovieService(IMovieRepository movieRepository) : IMovieService
+{
+    private readonly IMovieRepository _movieRepository = movieRepository;
+
+    public async Task CreateMovieAsync(MovieDto movieDTO)
+    {
+        var movie = new Movie(movieDTO.Id, movieDTO.Title, movieDTO.Director, movieDTO.Price);
+
+        await _movieRepository.AddAsync(movie);
+        await _movieRepository.SaveChangesAsync();
+    }
+
+    public async Task DeleteMovieAsync(Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+
+        if (movie is not null)
+        {
+            await _movieRepository.DeleteAsync(movie);
+            await _movieRepository.SaveChangesAsync();
+        }
+    }
+
+    public async Task<MovieDto> GetMovieByIdAsync(Guid id)
+    {
+        var movie = await _movieRepository.GetByIdAsync(id);
+
+        // TODO: handle not found case
+
+        return new MovieDto(movie.Id, movie.Title, movie.Director, movie.Price);
+    }
+
+    public async Task<List<MovieDto>> GetMoviesAsync()
+    {
+        var movies = (await _movieRepository.GetAsync())
+                     .Select(movie => new MovieDto(movie.Id, movie.Title, movie.Director, movie.Price))
+                     .ToList();
+
+        // TODO: add mappers
+        return movies;
+    }
+
+    public async Task UpdateMoviePriceAsync(Guid movieId, decimal newPrice)
+    {
+        var movie = await _movieRepository.GetByIdAsync(movieId);
+
+        // TODO: handle not found case
+
+        movie.UpdatePrice(newPrice);
+        await _movieRepository.SaveChangesAsync();
+    }
+}
